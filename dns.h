a78b9f8896d35ba22ffa8b7a92b11111451a12b5
githubuseronline <kilomaymany@rambler.ru>
2017-03-09 20:35:06 +0200

    Add files via upload

---
+++ dns.h
@@ -0,0 +1,10 @@
+//
+// Created by neit on 09.03.17.
+//
+
+#ifndef START_DNS_H
+#define START_DNS_H
+
+int daemonize(void);
+
+#endif //START_DNS_H
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dns.h"
+
+int main()
+{
+    puts("therewillbednsproxy\n");
+    return 0;
+}
+
+
+
+
